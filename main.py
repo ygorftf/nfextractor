21b6074bd7f78f1d2c3a787aa027bb79dab1ec3a
Ygor Torres <ygorftf@gmail.com>
2024-05-07 01:36:57 -0300

    main with dialogs

---
+++ main.py
@@ -0,0 +1,10 @@
+from tkinter import filedialog
+from packages import find_nf as find
+
+def main():
+    with filedialog.askopenfilename(title="Selecione o arquivo para ser extraido", filetypes=[('portable document format','*.pdf')]) as open:
+      with filedialog.asksaveasfile(title="Selecione a pasta para salvar o arquivo", defaultextension=".txt", filetypes=[('text','*.txt')]) as save:
+        save.write(find.find_nf(open))
+  
+if __name__ == "__main__":
+    main()
