0daac1838921ef5b6e337a75985aff82ef6b1641
Ygor Torres <ygorftf@gmail.com>
2024-05-07 01:38:58 -0300

    util directory added with find_nf.py

---
+++ util/find_nf.py
@@ -0,0 +1,21 @@
+import PyPDF2
+
+def find_nf(path):
+    txt = ""
+    with open(path, "rb") as pdf:
+        reader = PyPDF2.PdfReader(pdf)
+        for i in range(len(reader.pages)):
+            page = reader.pages[i]
+            text = page.extract_text()
+            x = text.find('NF-e',0)
+            test = True
+            while test:
+                y = x + 14
+                txt += f"{text[x:y]}\n"
+                x = text.find('NF-e',y)
+                if x == -1:
+                    test = False
+        return txt
+
+if __name__ == "__find_nf__":
+    find_nf()
