e7119828837ffb28658fb152efaad4b77d373361
Ygor Torres <ygorftf@gmail.com>
2024-05-08 22:09:57 -0300

    Loop added + try except/bin/python3

--- main.py
+++ main.py
@@ -2,9 +2,14 @@ from tkinter import filedialog
 from util import find_nf as find
 
 def main():
-    with filedialog.askopenfilename(title="Selecione o arquivo para ser extraido", filetypes=[('portable document format','*.pdf')]) as open:
-      with filedialog.asksaveasfile(title="Selecione a pasta para salvar o arquivo", defaultextension=".txt", filetypes=[('text','*.txt')]) as save:
+  con = True
+  while con:
+    try:
+      open = filedialog.askopenfilename(title="Selecione o arquivo para ser extraido", filetypes=[('portable document format','*.pdf')])
+      with filedialog.asksaveasfile(title="Selecione a pasta para salvar o arquivo", filetypes=[('Text archive','*.txt'),('Comma separeted file', '*.csv')]) as save:
         save.write(find.find_nf(open))
+    except TypeError:
+      con = False
   
 if __name__ == "__main__":
     main()
